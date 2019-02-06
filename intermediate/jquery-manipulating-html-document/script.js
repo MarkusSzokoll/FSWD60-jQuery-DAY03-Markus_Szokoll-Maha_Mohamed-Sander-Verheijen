e6e7aca92915dc26553a969122fcc056f49dfe41
Sander Verheijen <sanderverheijen@outlook.com>
2019-02-06 10:26:37 +0100

    adds intermediate in script.js (also data-type tags in products*.html)

---
+++ intermediate/jquery-manipulating-html-document/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	$("div[data-type='notebook']").css({"background-color": "red"})
+	$("div[data-type='phone']").css({"background-color": "green"})
+	$("div[data-type='tablet']").css({"background-color": "blue"})
+});
