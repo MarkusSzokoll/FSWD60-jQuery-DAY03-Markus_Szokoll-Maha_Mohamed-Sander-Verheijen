556c8e99b8529b06d734bb0987bd251e98392c7e
Sander Verheijen <sanderverheijen@outlook.com>
2019-02-06 10:57:34 +0100

    adds comments for intermediate and challenge

--- intermediate/jquery-manipulating-html-document/script.js
+++ intermediate/jquery-manipulating-html-document/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	//Added data-type to all product divs, selects and sets background color
 	$("div[data-type='notebook']").css({"background-color": "red"})
 	$("div[data-type='phone']").css({"background-color": "green"})
 	$("div[data-type='tablet']").css({"background-color": "blue"})
