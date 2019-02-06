d4cad099c1e8858681a71664632bcb73b38fbe7c
Sander Verheijen <sanderverheijen@outlook.com>
2019-02-06 15:43:38 +0100

    adds angle calculation but it doesn't work

--- advanced/myscript.js
+++ advanced/myscript.js
@@ -1,17 +1,31 @@
 $(document).ready(function() {
 	//Sets absolute position to enable movement by left and top css atts
-	$("#moveme").css({"position":"absolute"})
+	$("#moveme").css({"position":"absolute",
+                    "left":1,
+                    "top":1
+    });
 	//When the mouse moved do:
     $(document).mousemove(function (event) {
     	//Store values for use later
-    	tempx = event.pageX-120;
-    	tempy = event.pageY-120;
+    	mousex = event.pageX;
+    	mousey = event.pageY;
+        planex = $("#moveme").position().left;
+        planey = $("#moveme").position().top;
+        //Its broken but it shouldn't be please kill me
+        // lengthx = Math.sqrt(Math.pow(planex,2)+Math.pow(mousex,2));
+        // lengthy = Math.sqrt(Math.pow(planey,2)+Math.pow(mousey,2));
+        // dotProd = (mousex*planex)+(mousey*planey);
+        // toArcCos = (dotProd)/(lengthx*lengthy);
+        // angle = (Math.acos(toArcCos))*(180/Math.PI);
+        // console.log(angle + " " + lengthx + " " + lengthy + " " + dotProd)
     	//Stop() cancels previous animation, animate to mouse by setting left and top to mouse positions
         $("#moveme").stop()
         			.animate({
-        				"left": tempx,
-        				"top": tempy
-        			}, "slow", "linear");
+        				"left": mousex-120,
+        				"top": mousey-120
+        			}, "slow", "linear")
+        //             .css({'transform': 'rotate('+angle+'deg)'});
+
     });
     //When the mouse clicks on the plane
     $("#moveme").mousedown(function() {
@@ -22,7 +36,7 @@ $(document).ready(function() {
     				}, "slow", "linear")
     				.fadeOut()
 					.animate({
-						"left": tempx, "top": tempy
+						"left": mousex, "top": mousey
 					}, "fast", "linear")
 					.fadeIn();
     });
